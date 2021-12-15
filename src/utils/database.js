12bfb27b6d7fc9aa62fc8ce59e22f58ad4b35b67
ivfite27 <91913247+ivfite27@users.noreply.github.com>
2021-12-15 12:52:14 +0100

    Update database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,7 +1,6 @@
 const Sequelize = require("sequelize");
 const connection =
-  process.env.CONNECTION ||
-  "postgres://postgres:14xy68NAIV@localhost:5432/webshop";
+  process.env.CONNECTION ;
 
 const db = new Sequelize(connection);
 
