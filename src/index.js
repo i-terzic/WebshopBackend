ca306ce14413381e10fa05385250e9a45299121d
ivfite27 <i.terzic@gmx.net>
2022-01-14 17:38:27 +0100

    update /src/index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ const api = require('./app');
 const port = process.env.PORT || 5000;
 
 api.listen(port, () => {
+  /* eslint no-console: 0 */
   console.log(`Listening at: http://localhost:${port} `);
 });
